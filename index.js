e322f98a3c9b81bf8ad9eb5ef805d7f2111dea7d
git <gabbybailu@gmail.com>
2023-10-10 14:14:56 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+//Write a function findMatchig(), this function takes 2 parameters, an array with the drivers and a string, and returns the list with the drivers in a string
+function findMatching(drivers, string){
+    return drivers.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === string.toLowerCase()
+    );
+}
+//
+function fuzzyMatch(drivers,string) {
+   return drivers.filter(
+    (possibleMatch) =>
+    possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0 
+   );
+    }
+    //
+function matchName(drivers,string){
+    return drivers.filter((record) => record.name === string);
+}
+
+
